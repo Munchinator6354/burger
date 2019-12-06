b919b7e26babe9c274eabe62aa9a251713d0427f
Ryan Isaacson <munchinator6354@gmail.com>
2019-12-05 23:23:11 -0800

    created initial structure files, created server.js, created basic schema and seeds for mysql db and populated the table

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Trouble", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("CheeseyMcCheeseCheese", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon_Bacon_Ain't_No_Fakin'", true);
